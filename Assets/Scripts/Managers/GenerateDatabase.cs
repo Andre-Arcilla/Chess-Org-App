0caf3337dc0a9969d95df8aa96b52128c6a50e08
Andre-Arcilla <aaarcilla123@gmail.com>
2025-10-25 01:17:18 +0800

    Build APK

--- Assets/Scripts/Managers/GenerateDatabase.cs
+++ Assets/Scripts/Managers/GenerateDatabase.cs
@@ -1,11 +1,19 @@
-using SQLite;
+using SQLite4Unity3d;
 using System.IO;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class GenerateDatabase : MonoBehaviour
 {
     [Header("data")]
     [SerializeField] private string saveFileName = "Hoshiyomi_ChessOrg.db";
+
+    [SerializeField] private GameObject profilePage;
+    [SerializeField] private TextMeshProUGUI studName;
+    [SerializeField] private TextMeshProUGUI studNum;
+    [SerializeField] private TextMeshProUGUI rating;
+    [SerializeField] private TextMeshProUGUI puzzles;
 
     void Start()
     {
@@ -17,7 +25,7 @@ public class GenerateDatabase : MonoBehaviour
         database.Execute(@"
             CREATE TABLE IF NOT EXISTS Profiles (
                 UserID INTEGER NOT NULL DEFAULT 1 CHECK(UserID >= 0),
-                StudNum TEXT NOT NULL UNIQUE COLLATE NOCASE,
+                StudNum TEXT NOT NULL COLLATE NOCASE,
                 Password TEXT NOT NULL DEFAULT '12345' COLLATE BINARY,
                 Name TEXT NOT NULL DEFAULT 'John Smith' COLLATE NOCASE,
                 College TEXT NOT NULL DEFAULT 'CCIS' COLLATE NOCASE,
@@ -95,5 +103,42 @@ public class GenerateDatabase : MonoBehaviour
 	            PRIMARY KEY(MmbrID AUTOINCREMENT)
             );
         ");
+
+        // DEBUG TEST
+        database.Execute(@"
+            INSERT INTO Profiles (StudNum, Password, Name, College, Rating, Puzzles, Role) 
+            VALUES ('A12346169', '123', 'Andre Arcilla', 'CCIS', 410, 10, 'Member');
+        ");
+
+        int oldIndex = profilePage.transform.GetSiblingIndex();
+        profilePage.transform.SetAsFirstSibling();
+        profilePage.SetActive(true);
+        Profiles profile = database.Table<Profiles>().FirstOrDefault();
+
+        studName.text = profile.StudName;
+        studNum.text = profile.Name;
+        rating.text = profile.Rating.ToString();
+        puzzles.text = profile.Puzzles.ToString();
+        profilePage.SetActive(false);
+        profilePage.transform.SetSiblingIndex(oldIndex);
     }
+}
+
+public class Profiles
+{
+    public int UserID { get; set; }
+
+    public string Name { get; set; }
+
+    public string Password { get; set; }
+
+    public string StudName { get; set; }
+
+    public string College { get; set; }
+
+    public int Rating { get; set; }
+
+    public int Puzzles { get; set; }
+
+    public string Role { get; set; }
 }
